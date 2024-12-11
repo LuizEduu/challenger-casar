3ffcc2c43331855c8af871c923d153afe1620b40
Luiz <l.eduardo8316@gmail.com>
2024-12-11 13:34:13 -0300

    test: e2e test to health check controller

---
+++ src/infra/http/controllers/health-check.controller.e2e-spec.ts
@@ -0,0 +1,29 @@
+import { AppModule } from '@/infra/app.module'
+import { DatabaseModule } from '@/infra/database/database.module'
+import { INestApplication } from '@nestjs/common'
+import { Test } from '@nestjs/testing'
+import request from 'supertest'
+
+describe('HealthController (E2E)', () => {
+  let app: INestApplication
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule, DatabaseModule],
+    }).compile()
+
+    app = moduleRef.createNestApplication()
+    await app.init()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  test('[GET] /health', async () => {
+    const response = await request('http://localhost:8080').get('/health') // chama o nginx para validar que os servers estão ok
+
+    expect(response.statusCode).toEqual(200)
+    expect(response.text).toEqual('OK')
+  })
+})
